a093ef6b1fb528817a4571ed518fde2d5e5594a2
Clazmaina <claywangari@gmail.com>
2025-02-27 19:05:14 +0200

    Managing permissions and groups in Django.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.core.exceptions import PermissionDenied
+from .models import Book  # Import your Book model
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    # Add your book creation logic here
+    # ...
+    return render(request, 'bookshelf/book_create.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    # Add your book editing logic here
+    # ...
+    return render(request, 'bookshelf/book_edit.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    # Add your book deletion logic here
+    # ...
+    return render(request, 'bookshelf/book_delete.html')
