8080a01398cdee27ffcb839028a2355c6a297e0e
Clazmaina <claywangari@gmail.com>
2025-02-27 19:40:01 +0200

    task 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -27,3 +27,17 @@ def book_delete(request, book_id):
     # Add your book deletion logic here
     # ...
     return render(request, 'bookshelf/book_delete.html')
+
+from django.shortcuts import render
+from .forms import ExampleForm  # Import ExampleForm
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            # ...
+            return render(request, 'bookshelf/form_example.html', {'form': form})
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
