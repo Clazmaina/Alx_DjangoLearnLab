d158fc30d05f42485041e0958f37c585b8fca2b1
Clazmaina <claywangari@gmail.com>
2025-03-13 15:42:22 +0300

    Implementing filtering, searching, and ordering in django REST framework.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,27 +1,26 @@
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 from .models import Book
 from django.urls import reverse_lazy
-from .forms import BookForm
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .forms import BookForm 
 
 class BookListView(ListView):
     model = Book
-    template_name = 'book_list.html'
+    template_name = 'book_list.html' 
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'book_detail.html'
+    template_name = 'book_detail.html' 
 
 class BookCreateView(CreateView):
     model = Book
     form_class = BookForm
-    template_name = 'book_form.html'
+    template_name = 'book_form.html' 
     success_url = reverse_lazy('book-list')
 
 class BookUpdateView(UpdateView):
     model = Book
     form_class = BookForm
-    template_name = 'book_form.html'
+    template_name = 'book_form.html' 
     success_url = reverse_lazy('book-list')
 
 class BookDeleteView(DeleteView):
