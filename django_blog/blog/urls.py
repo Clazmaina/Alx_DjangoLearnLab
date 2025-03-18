7afb959d937ec1a036dc66943baa061e98c8e03b
Clazmaina <claywangari@gmail.com>
2025-03-18 20:32:04 +0300

    Adding comment functionality to blog posts.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('posts/<int:pk>/comments/new/', views.PostDetailView.as_view(), name='comment_new'),
+    path('posts/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_new'),
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -88,6 +88,20 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'  # You can create a separate template or reuse comment_form.html
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', args=[self.kwargs['post_pk']])
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
