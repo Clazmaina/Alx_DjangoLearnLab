6d39fa34ed66c51fdc254b096bb992a96f0e3f8c
Clazmaina <claywangari@gmail.com>
2025-03-18 16:23:19 +0300

    Creating blog post management features.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
-from .forms import CustomUserCreationForm  # Corrected import
+from .forms import CustomUserCreationForm
 
 def register(request):
     if request.method == 'POST':
@@ -25,3 +25,46 @@ def profile(request):
     else:
         form = UserChangeForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']  
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')  # Redirect to post list after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
