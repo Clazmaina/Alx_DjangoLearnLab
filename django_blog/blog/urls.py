0e415ee7ec639b9f49c7cab4009120a5f68bf09e
Clazmaina <claywangari@gmail.com>
2025-03-18 21:28:32 +0300

    Implementing advanced features:Tagging and search functionality.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
     path('tags/<slug:tag_slug>/', views.PostListView.as_view(), name='post_list_by_tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post_list_by_tag'),
     path('search/', views.PostSearchView.as_view(), name='post_search'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -162,3 +162,19 @@ class PostSearchView(ListView):
         context = super().get_context_data(**kwargs)
         context['query'] = self.request.GET.get('q')
         return context
+    
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list_by_tag.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_slug = self.kwargs.get('tag_slug')
+        context['tag'] = get_object_or_404(Tag, slug=tag_slug)
+        return context
