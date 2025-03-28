dd5b84f97a24bf10e868cc2a400026d0d22fbb04
Clazmaina <claywangari@gmail.com>
2025-03-28 15:18:37 +0300

    task 2

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    
+    follower = models.ManyToManyField('self', symmetrical=False, blank=True)
+
     def __str__(self):
         return self.username
 
