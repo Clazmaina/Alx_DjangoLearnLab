404f10d1c04abfad0e48e406982f01e16828839d
Clazmaina <claywangari@gmail.com>
2025-03-28 14:39:51 +0300

    Implementing posts and comments functionality.

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
+    path('api/', include('accounts.urls')),
 ]
